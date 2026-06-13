941876c3cd6973be870fd4fe4d5a457dd53c8cdf
varenyzc <465057895@qq.com>
2026-06-13 16:35:51 +0800

    Initial tap: add agentbar cask

---
+++ Casks/agentbar.rb
@@ -0,0 +1,26 @@
+cask "agentbar" do
+  version "0.1.4"
+  sha256 "2fe9ef0fb6ce8eb09c280b39227dbc2c47d3e1f6b0125128571c0fecda66a085"
+
+  url "https://github.com/varenyzc1/agentbar/releases/download/v#{version}/AgentBar-macos.zip",
+      verified: "github.com/varenyzc1/agentbar/"
+  name "AgentBar"
+  desc "macOS menu bar app for tracking local AI coding assistant usage"
+  homepage "https://github.com/varenyzc1/agentbar"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "AgentBar.app"
+
+  uninstall quit: "com.agentbar.app"
+
+  zap trash: [
+    "~/Library/Application Support/AgentBar",
+    "~/Library/Preferences/com.agentbar.app.plist",
+  ]
+end
